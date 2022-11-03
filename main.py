cdd10db21f2ab26d15a8a87d0f66b58f8ca7a805
Sung Su Bae <22442453+sungsubae@users.noreply.github.com>
2022-11-03 02:01:33 +0000

    Changed main.py's contect_data to baseline code

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 from src import seed_everything
 
-#from src.data import context_data_load, context_data_split, context_data_loader
-from src.ksy_data import context_data_load, context_data_split, context_data_loader
+from src.data import context_data_load, context_data_split, context_data_loader
+#from src.ksy_data import context_data_load, context_data_split, context_data_loader
 from src.data import dl_data_load, dl_data_split, dl_data_loader
 from src.data import image_data_load, image_data_split, image_data_loader
 from src.data import text_data_load, text_data_split, text_data_loader
